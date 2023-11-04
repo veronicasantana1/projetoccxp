2b0ac6d39c1b8094c6f10e05d393ed7cb5b82d4e
unknown <santanaveronica858@gmail.com>
2023-11-04 19:15:01 -0300

    Final

--- main.js
+++ main.js
@@ -1,6 +1,78 @@
+const ingressos = [];
+
 function addRedBorder(id) {
-  Element = document.querySelector("#" + id);
-  Element.style.border = "5px solid red";
+  element = document.querySelector("#" + id);
+  element.style.border = "5px solid red";
 }
 
-addRedBorder("sexta");
+function highlightCard(cardId) {
+  var element = document.querySelector(cardId);
+  element.classList.toggle("card-highlight");
+}
+
+function checkKeyboardCode() {
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      var name = event.key;
+      var code = event.code;
+      alert(`Tecla Pressionada ${name} \r\n Key code: ${code}`);
+    },
+    false
+  );
+}
+
+function addKeyboardEventListeners() {
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      var ingresso1 = document.getElementById("quinta");
+      var ingresso2 = document.getElementById("sexta");
+      var ingresso3 = document.getElementById("sabado");
+      var ingresso4 = document.getElementById("domingo");
+
+      var code = event.code;
+      if (code == "Digit1") {
+        ingresso1.classList.toggle("card-highlight");
+        ingresso2.classList.remove("card-highlight");
+        ingresso3.classList.remove("card-highlight");
+        ingresso4.classList.remove("card-highlight");
+      }
+
+      if (code == "Digit2") {
+        ingresso1.classList.remove("card-highlight");
+        ingresso2.classList.toggle("card-highlight");
+        ingresso3.classList.remove("card-highlight");
+        ingresso4.classList.remove("card-highlight");
+      }
+
+      if (code == "Digit3") {
+        ingresso1.classList.remove("card-highlight");
+        ingresso2.classList.remove("card-highlight");
+        ingresso3.classList.toggle("card-highlight");
+        ingresso4.classList.remove("card-highlight");
+      }
+
+      if (code == "Digit4") {
+        ingresso1.classList.remove("card-highlight");
+        ingresso2.classList.remove("card-highlight");
+        ingresso3.classList.remove("card-highlight");
+        ingresso4.classList.toggle("card-highlight");
+      }
+    },
+    false
+  );
+}
+
+function selectCard(selector) {
+  var element = document.querySelector(selector);
+  element.classList.toggle("card-selected");
+  if (ingressos.includes(selector)) ingressos.pop(selector);
+  else ingressos.push(selector);
+}
+
+function showSelectedCards() {
+  if (ingressos.lenght > 0) alert("Ingressos Selecionados:" + ingressos);
+}
+
+addKeyboardEventListeners();
